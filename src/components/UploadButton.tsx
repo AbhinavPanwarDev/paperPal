2c3edabad746f81d1b5a3a442a8f47a1b6088754
Abhi <abhinav.panwarr@gmail.com>
2024-10-09 00:28:06 +0530

    uploadButton/uploadThing

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,6 +3,58 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
+import Dropzone from "react-dropzone";
+import { Cloud, File } from "lucide-react";
+
+const UploadDropzone = () => {
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFile) => {
+        console.log(acceptedFile);
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="border h-64 m-4 border-dashed border-gray-400 rounded-lg"
+        >
+          <div className="flex-items-center justify-center h-full w-full">
+          <label
+              htmlFor='dropzone-file'
+              className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
+              <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+                <Cloud className='h-8 w-8 text-zinc-700 mb-4' />
+                <p className='mb-2 text-md text-zinc-700'>
+                  <span className='font-semibold'>
+                    Click to upload
+                  </span>{' '}
+                  or drag and drop your files here.
+                </p>
+                <p className='text-sm text-zinc-500'>
+                  PDF (up to 4MB)
+                </p>
+              </div>
+
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-items-center">
+                    <File className="h-4 w-4 text-blue-500" />
+                  </div>
+                  <div className="px-3 py-2 h-full text-sm truncate">
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+
+              
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
+};
 
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -16,15 +68,13 @@ const UploadButton = () => {
         }
       }}
     >
-        <DialogTrigger onClick={() => setIsOpen(true)} asChild>
-            <Button>Upload PDF</Button>
-        </DialogTrigger>
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
 
-        <DialogContent>
-            Select the files to upload for chatting...
-        </DialogContent>
-
-        
+      <DialogContent>
+        <UploadDropzone />
+      </DialogContent>
     </Dialog>
   );
 };
