390c0960bfc325f64802d977d9fd0532100c2601
Abhi <abhinav.panwarr@gmail.com>
2024-10-09 00:28:06 +0530

    uploadButton/uploadThing

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -2,8 +2,12 @@
 
 const PdfRenderer = () => {
   return (
-    <div>
-      PDF here
+    <div  className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-300 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">
+            pdf Options bar
+        </div>
+      </div>
     </div>
   )
 }
