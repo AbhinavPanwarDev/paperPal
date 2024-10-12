f4faea0532c7972bba8385cf30de3da332f2e68e
Abhi <abhinav.panwarr@gmail.com>
2024-10-12 15:07:05 +0530

    getFile and Polling

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
@@ -6,13 +7,18 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-     
+      
+      const {getUser} = getKindeServerSession()
+
+      const user = await getUser()
+
+      if(!user || !user.id) throw new Error('Unauthorized')      
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return {};
+      return {userId: user.id};
     })
     .onUploadComplete(async ({ metadata, file }) => {}),
 
