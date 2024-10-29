4e3375aa5d020ce25fe6e049784785927966db2f
Abhi <abhinav.panwarr@gmail.com>
2024-10-29 17:42:01 +0530

    message realtime streaming and other designChanges

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -2,6 +2,7 @@ import { ReactNode, createContext, useRef, useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
 import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
+import { toast } from "@/hooks/use-toast";
 
 type StreamResponse = {
   addMessage: () => void;
@@ -102,6 +103,92 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
           ) ?? [],
       }
     },
+
+    //realtime streaming of message received by AI
+
+    onSuccess: async (stream) => {
+      setIsLoading(false)
+
+      if (!stream) {
+        return toast({
+          title: 'There was a problem sending this message',
+          description:
+            'Please refresh this page and try again',
+          variant: 'destructive',
+        })
+      }
+
+      const reader = stream.getReader()
+      const decoder = new TextDecoder()
+      let done = false
+
+      // accumulated response
+      let accResponse = ''
+
+      while (!done) {
+        const { value, done: doneReading } =
+          await reader.read()
+        done = doneReading
+        const chunkValue = decoder.decode(value)
+
+        accResponse += chunkValue
+
+        // append chunk to the actual message
+        utils.getFileMessages.setInfiniteData(
+          { fileId, limit: INFINITE_QUERY_LIMIT },
+          (old) => {
+            if (!old) return { pages: [], pageParams: [] }
+
+            let isAiResponseCreated = old.pages.some(
+              (page) =>
+                page.messages.some(
+                  (message) => message.id === 'ai-response'
+                )
+            )
+
+            let updatedPages = old.pages.map((page) => {
+              if (page === old.pages[0]) {
+                let updatedMessages
+
+                if (!isAiResponseCreated) {
+                  updatedMessages = [
+                    {
+                      createdAt: new Date().toISOString(),
+                      id: 'ai-response',
+                      text: accResponse,
+                      isUserMessage: false,
+                    },
+                    ...page.messages,
+                  ]
+                } else {
+                  updatedMessages = page.messages.map(
+                    (message) => {
+                      if (message.id === 'ai-response') {
+                        return {
+                          ...message,
+                          text: accResponse,
+                        }
+                      }
+                      return message
+                    }
+                  )
+                }
+
+                return {
+                  ...page,
+                  messages: updatedMessages,
+                }
+              }
+
+              return page
+            })
+
+            return { ...old, pages: updatedPages }
+          }
+        )
+      }
+    },
+
 
     onError: (_, __, context) => {
       setMessage(backupMessage.current)
