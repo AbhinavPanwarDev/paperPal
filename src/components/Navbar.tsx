7e91d01edf83dbee854e66451e2d314936e439e8
Abhi <abhinav.panwarr@gmail.com>
2024-11-02 23:48:23 +0530

    mobile Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
 import MaxWidthWraper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { LoginLink, RegisterLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight } from "lucide-react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
+
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/80 transition-all backdrop-blur-lg">
       <MaxWidthWraper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            <span>.Paper.Pal.</span>
+            <span>paperPal</span>
           </Link>
+
+          <MobileNav isAuth={!!user} />
 
           <div className="hidden items-center space-x-4 sm:flex">
             <>
